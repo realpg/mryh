10484563bd1936f5cb9fe372e01bf71c451e2a3b
t89210827 <15142016025@163.com>
2018-08-21 13:46:35 +0800

    Acker

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <div v-bind:style="{ 'min-height': windowHeight + 'px' }" style="background:#f4f4f4;">
-    <img src='../../../static/images/1.png' style='width:750rpx;height:350rpx;'></img>
+<div v-bind:style="{ 'min-height': windowHeight + 'px' }" style="background:#f4f4f4;">
+  <img src='../../../static/images/1.png' style='width:750rpx;height:350rpx;'></img>
 
   <div style='margin:10rpx 25rpx;'>
     <div style='text-align:right;margin-top:20rpx;'>
@@ -13,31 +13,47 @@
     </div>
   </div>
 
-  <div style="background:#F5f5f5;height:20rpx;width:750rpx;"></div>
+  <div style="background:#fff;height:20rpx;width:750rpx;margin-top:35rpx;"></div>
 
+  <div style="line-height:130rpx;text-align:center;font-size:32rpx;font-weight:500;">挑战式艺术成长</div>
 
-    <div class="bottom flex-row">
-
-      <div @click="switchNavigation" id="1" class="tab">
-        <img src="../../../static/images/bottom/easel_n.png" style="width:50rpx;height:50rpx;" alt="">
-        <div style="font-size:26rpx;">首页</div>
-      </div>
-
-      <div @click="switchNavigation" id="2" class="tab add">
-        <div class="picture center-style" style="background:#E21918;width:80rpx;height:80rpx;border-radius: 50%;">
-          <img src="../../../static/images/bottom/easel_n.png" style="width:50rpx;height:50rpx;" alt="">
-        </div>
-        <div style="width:50rpx;height:50rpx;"></div>
-        <div style="font-size:26rpx;">每天一画</div>
-      </div>
-
-      <div @click="switchNavigation" id="3" class="tab">
-        <img src="../../../static/images/bottom/easel_n.png" style="width:50rpx;height:50rpx;" alt="">
-        <div style="font-size:26rpx;">我的</div>
-      </div>
-
+  <div class="flex-row">
+    <div style="width:25%;height:187.5rpx;display:flex;flex-direction: column;justify-content:center;align-items: center;">
+        <img src="../../../static/images/bottom/easel_n.png" style="width:80rpx;height:80rpx;">
+        <div style="font-size:30rpx;margin-top:5rpx;">奖金激励</div>
+    </div>
+      <div style="width:25%;height:187.5rpx;display:flex;flex-direction: column;justify-content:center;align-items: center;">
+        <img src="../../../static/images/bottom/easel_n.png" style="width:80rpx;height:80rpx;">
+        <div style="font-size:30rpx;margin-top:5rpx;">激发兴趣</div>
+    </div>
+      <div style="width:25%;height:187.5rpx;display:flex;flex-direction: column;justify-content:center;align-items: center;">
+        <img src="../../../static/images/bottom/easel_n.png" style="width:80rpx;height:80rpx;">
+        <div style="font-size:30rpx;margin-top:5rpx;">契约约束</div>
+    </div>
+      <div style="width:25%;height:187.5rpx;display:flex;flex-direction: column;justify-content:center;align-items: center;">
+        <img src="../../../static/images/bottom/easel_n.png" style="width:80rpx;height:80rpx;">
+        <div style="font-size:30rpx;margin-top:5rpx;">快速成长</div>
     </div>
   </div>
+
+  <div class="bottom flex-row">
+    <div @click="switchNavigation" id="1" class="tab">
+      <img src="../../../static/images/bottom/easel_n.png" style="width:50rpx;height:50rpx;" alt="">
+      <div style="font-size:26rpx;">首页</div>
+    </div>
+    <div @click="switchNavigation" id="2" class="tab add">
+      <div class="picture center-style" style="background:#E21918;width:80rpx;height:80rpx;border-radius: 50%;">
+        <img src="../../../static/images/bottom/easel_n.png" style="width:50rpx;height:50rpx;" alt="">
+      </div>
+      <div style="width:50rpx;height:50rpx;"></div>
+      <div style="font-size:26rpx;">每天一画</div>
+    </div>
+    <div @click="switchNavigation" id="3" class="tab">
+      <img src="../../../static/images/bottom/easel_n.png" style="width:50rpx;height:50rpx;" alt="">
+      <div style="font-size:26rpx;">我的</div>
+    </div>
+  </div>
+</div>
 
 </template>
 
@@ -80,10 +96,9 @@ export default {
 };
 </script>
 
-<style scoped>
-page {
-  background: #f4f4f4;
-}
+
+<style lang="scss" scoped>
+$color1: #e21918;
 
 .bottom {
   position: fixed;
